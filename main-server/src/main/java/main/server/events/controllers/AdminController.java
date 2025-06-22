8dd686789741b4d2579ae9aa792a3f779a3a7e9b
Andrey Potokin <potokin.and@yandex.ru>
2025-06-22 12:58:38 +0300

    Тесты Постмана по Category пройдены успешно

--- main-server/src/main/java/main/server/events/controllers/AdminController.java
+++ main-server/src/main/java/main/server/events/controllers/AdminController.java
@@ -1,28 +1,28 @@
-package main.server.events.controllers;
-
-import lombok.extern.slf4j.Slf4j;
-import main.server.events.dto.EventShortDto;
-import main.server.events.dto.UpdateEventAdminRequest;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController("/admin/events")
-@Slf4j
-public class AdminController {
-
-    @GetMapping
-    public List<EventShortDto> getEvents() {
-        return null;
-    }
-
-    @PostMapping("/{eventId}")
-    public EventShortDto updateEvent(@RequestBody UpdateEventAdminRequest updateEventAdminRequest,
-                                   @RequestParam Long eventId) {
-        return null;
-    }
-}
+//package main.server.events.controllers;
+//
+//import lombok.extern.slf4j.Slf4j;
+//import main.server.events.dto.EventShortDto;
+//import main.server.events.dto.UpdateEventAdminRequest;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController("/admin/events")
+//@Slf4j
+//public class AdminController {
+//
+//    @GetMapping
+//    public List<EventShortDto> getEvents() {
+//        return null;
+//    }
+//
+//    @PostMapping("/{eventId}")
+//    public EventShortDto updateEvent(@RequestBody UpdateEventAdminRequest updateEventAdminRequest,
+//                                   @RequestParam Long eventId) {
+//        return null;
+//    }
+//}
