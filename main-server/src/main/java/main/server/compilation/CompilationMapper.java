429b2853ad55fea4de2414cf099449a44018ab46
Gosling <popkornuh@gmail.com>
2025-06-25 00:18:47 +0300

    Compilation

--- main-server/src/main/java/main/server/compilation/CompilationMapper.java
+++ main-server/src/main/java/main/server/compilation/CompilationMapper.java
@@ -7,8 +7,10 @@ import main.server.compilation.model.Compilation;
 import main.server.events.mapper.EventMapper;
 import main.server.events.model.EventModel;
 import main.server.events.repository.EventRepository;
+import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 
 import java.util.List;
 import java.util.Set;
@@ -18,22 +20,27 @@ import java.util.stream.Collectors;
 public interface CompilationMapper {
 
     @Mapping(target = "id", ignore = true)
-    @Mapping(target = "events", source = "events")
-    Compilation toEntity(NewCompilationDto dto, @org.mapstruct.MappingTarget MapperContext context);
+    @Mapping(source = "events", target = "events", qualifiedByName = "mapEventIds")
+    Compilation toEntity(NewCompilationDto dto, @Context MapperContext context);
 
     CompilationDto toDto(Compilation compilation);
 
     List<CompilationDto> toDtoList(List<Compilation> compilations);
 
-
-    default Set<EventModel> map(Set<Long> eventIds, @org.mapstruct.MappingTarget MapperContext context) {
+    @Named("mapEventIds")
+    default Set<EventModel> map(Set<Long> eventIds, @Context MapperContext context) {
+        if (eventIds == null) {
+            return null;
+        }
         return eventIds.stream()
                 .map(id -> mapToEventModel(id, context))
                 .collect(Collectors.toSet());
     }
 
-    default EventModel mapToEventModel(Long eventId, @org.mapstruct.MappingTarget MapperContext context) {
-        return context.getEventRepository().findById(eventId).orElse(null); // Можно выбросить исключение при необходимости
+    default EventModel mapToEventModel(Long eventId, @Context MapperContext context) {
+        return (context != null && context.getEventRepository() != null)
+                ? context.getEventRepository().findById(eventId).orElse(null)
+                : null;
     }
 
     @Getter
