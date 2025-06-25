7f769d07aab003ee962d5dc96e7d8a24d0d2fc37
OlgaPegushina <pegushina.shine@yandex.ru>
2025-06-26 02:00:56 +0300

    исправления, связанные с request

--- main-server/src/main/java/main/server/request/service/RequestServiceImpl.java
+++ main-server/src/main/java/main/server/request/service/RequestServiceImpl.java
@@ -22,6 +22,7 @@ import main.server.user.model.User;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.LinkedHashMap;
@@ -182,6 +183,7 @@ public class RequestServiceImpl implements RequestService {
         ParticipationRequest participationRequest = new ParticipationRequest();
         participationRequest.setRequester(requester);
         participationRequest.setEvent(event);
+        participationRequest.setCreated(LocalDateTime.now());
 
         if (!event.getRequestModeration() || event.getParticipantLimit() == 0) {
             participationRequest.setStatus(RequestStatus.CONFIRMED);
